153860828d060e5bacb9d65b76560ee411c543ab
chris <cenne1986@qq.com>
2017-06-21 16:41:05 +0800

    修改mysql

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -7,32 +7,39 @@ import (
 	"errors"
 )
 
-var db *gorm.DB
+type Mysql struct {
+	db *gorm.DB
+}
+
+var (
+	mysql *Mysql
+)
 
 func NewConn(dataSource, prefix string) (*gorm.DB, error) {
 
-	if db != nil {
-		return db, nil
+	if mysql.db != nil {
+		return mysql.db, nil
 	}
 
 	setTablePrefix(prefix)
-	connection, err := gorm.Open("mysql", dataSource)
+	db, err := gorm.Open("mysql", dataSource)
 	if err != nil {
 		return nil, err
 	}
 
-	connection.DB().SetConnMaxLifetime(30 * time.Second)
+	db.DB().SetConnMaxLifetime(30 * time.Second)
+	mysql = &Mysql{
+		db,
+	}
 
-	connection = db
-
-	return connection, err
+	return mysql.db, err
 }
 
 func GetConn() (*gorm.DB, error){
-	if db == nil {
+	if mysql.db == nil {
 		return nil, errors.New("connection is not exist")
 	}
-	return db, nil
+	return mysql.db, nil
 }
 
 func setTablePrefix(prefix string) {
