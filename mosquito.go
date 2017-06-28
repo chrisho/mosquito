2f59e4498c969431847336066681b5e8655ec100
ggguang <ggguang@uufff.com>
2017-06-28 15:06:56 +0800

    2017-06-28

--- mosquito.go
+++ mosquito.go
@@ -1,14 +1,13 @@
 package mosquito
 
 import (
-	"net"
-	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc"
-	"github.com/chrisho/mosquito/helper"
-	"os"
-	"github.com/joho/godotenv"
-	"google.golang.org/grpc/credentials"
 	"github.com/chrisho/mosquito/zookeeper"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+	"net"
+	"os"
 )
 
 const envFile = "/config/conf.env"
@@ -25,7 +24,7 @@ func init() {
 	}
 }
 
-func GetServer() (*grpc.Server) {
+func GetServer() *grpc.Server {
 	var opts []grpc.ServerOption
 	creds, err := credentials.NewServerTLSFromFile(path+"/config/server.crt", path+"/config/server.key")
 	if err != nil {
@@ -40,69 +39,43 @@ func GetServer() (*grpc.Server) {
 
 func RunServer() {
 	listen_addr := zookeeper.GetServerAddress()
-	listen, err := net.Listen("tcp", listen_addr)
-	if err != nil {
-		grpclog.Error(err)
-	}
-	err = server.Serve(listen)
+	grpclog.Info("server address is ", listen_addr)
+
+	children, err := zookeeper.RegMicroServer()
 
 	if err != nil {
 		grpclog.Fatal(err)
 	}
-	_, err = zookeeper.RegMicroServer()
+	grpclog.Info("zookeeper servers is ", children)
+
+	listen, err := net.Listen("tcp", listen_addr)
+
+	if err != nil {
+		grpclog.Error(err)
+	}
+
+	err = server.Serve(listen)
 
 	if err != nil {
 		grpclog.Fatal(err)
 	}
 }
 
-func GetClientConn() (*grpc.ClientConn) {
-	address := helper.GetEnv("ServerAddress") + ":" + helper.GetEnv("ServerPort")
+func GetClientConn() *grpc.ClientConn {
+	address := zookeeper.GetServerAddress()
+	grpclog.Info("serverAddress is ", address)
 	var opts []grpc.DialOption
 	var creds credentials.TransportCredentials
 
-	creds, err := credentials.NewClientTLSFromFile("config/server.crt",
-		"sude365.com")
+	creds, err := credentials.NewClientTLSFromFile("config/server.crt", "sude365.com")
 
-	opts = append(opts, grpc.WithTransportCredentials(creds),
-		grpc.WithInsecure())
+	opts = append(opts, grpc.WithTransportCredentials(creds), grpc.WithInsecure())
 
 	conn, err := grpc.Dial(address, opts...)
+
 	if err != nil {
 		grpclog.Error(err)
 	}
 
 	return conn
 }
-
-/*
-func RunServer(serverAddr, serverPort string, processor thrift.TProcessor) (err error){
-	transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
-	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
-	address := serverAddr + ":" + serverPort
-	ssl := helper.GetEnv("SSL")
-
-	var transport thrift.TServerTransport
-	if ssl == "true" {
-		cfg := new(tls.Config)
-		if cert, err := tls.LoadX509KeyPair("config/server.crt", "config/server.key"); err == nil {
-			cfg.Certificates = append(cfg.Certificates, cert)
-		} else {
-			return err
-		}
-		transport, err = thrift.NewTSSLServerSocket(address, cfg)
-	} else {
-		transport, err = thrift.NewTServerSocket(address)
-	}
-
-	if err != nil {
-		return err
-	}
-	//handler := &serviceHandler.UserHandler{}
-	//processor := thriftHandler.NewSdUserServiceProcessor(handler)
-	server := thrift.NewTSimpleServer4(processor, transport, transportFactory, protocolFactory)
-
-	println("Starting the simple server... on ", address)
-	return server.Serve()
-}
-*/
