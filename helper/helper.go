8cd991ae963320fbdbbcfd9d64cbfbcf51c13a4f
ggguang <ggguang@uufff.com>
2017-07-01 19:11:16 +0800

    2017-07-01 helper add time to date || date to time

--- helper/helper.go
+++ helper/helper.go
@@ -9,6 +9,15 @@ import (
 	"github.com/chrisho/mosquito/utils"
 	"google.golang.org/grpc/grpclog"
 	"reflect"
+	"strconv"
+	"time"
+)
+
+const (
+	YYYYMMDDHHIISS = "2006-01-02 15:04:05"
+	YYYYMMDDHHII = "2006-01-02 15:04"
+	YYYYMMDDHH = "2006-01-02 15"
+	YYYYMMDD = "2006-01-02"
 )
 
 func GetEnv(key string) (value string) {
@@ -79,4 +88,30 @@ func GetServerAddress() (ipAddress string) {
 	}
 
 	return
+}
+
+// RFC3339 time string to format Y-m-d H:i
+func RFC3339Time2Date(timeStr, dateLayout string) string {
+	timestamp, _ := time.ParseInLocation(time.RFC3339, timeStr, time.Local)
+
+	return timestamp.Format(dateLayout)
+}
+
+// timestamp to format Y-m-d H:i:s
+func Date2Timestamp(dateLayout, date string) int64 {
+	localTime, _ := time.ParseInLocation(dateLayout, date, time.Local)
+
+	return localTime.Unix()
+}
+
+// timestamp to format Y-m-d H:i:s
+func Timestamp2Date(timestamp int64, dateLayout string) string {
+	return time.Unix(timestamp, 0).Format(dateLayout)
+}
+
+// string to int32
+func String2Int32(str string) int32 {
+	int64Str, _ := strconv.Atoi(str)
+
+	return int32(int64Str)
 }
