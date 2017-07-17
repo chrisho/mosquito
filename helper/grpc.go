c780aa3f2a452f11059e858e1434f0de0dd8dc04
ggguang <ggguang@uufff.com>
2017-07-17 17:57:56 +0800

    2017-07-17 grpc-error

--- helper/grpc.go
+++ helper/grpc.go
@@ -3,10 +3,9 @@ package helper
 import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	//"google.golang.org/grpc/grpclog"
 	"runtime"
 	"fmt"
-	_ "github.com/chrisho/mosquito/alilog"
+	//_ "github.com/chrisho/mosquito/alilog"
 	"github.com/sirupsen/logrus"
 )
 
@@ -16,7 +15,6 @@ func GrpcError(c codes.Code, format string) error {
 
 	file = fmt.Sprintf("error file : %v ( line : %d )", file, line)
 
-	//grpclog.Info(file)
 	logrus.Error(file)
 
 	return status.Errorf(c, format)
