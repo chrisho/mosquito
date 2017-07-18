276cb8b1b514729e12c4da6d22c4fa9faee1a06a
ggguang <ggguang@uufff.com>
2017-07-18 10:15:04 +0800

    2017-07-18 helper.random

--- helper/random.go
+++ helper/random.go
@@ -1,11 +1,38 @@
 package helper
 
 import (
+	"time"
+	"math/rand"
 	"strings"
-	"github.com/manveru/faker"
+	"strconv"
 )
 
-func RandomString()  {
-	strings.ToLower("A")
-	faker.New("zh-CN")
+func RandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	rs := make([]string, length)
+
+	for start := 0; start < length; start++ {
+		t := rand.Intn(2)
+		if t == 0 {
+			// A-Z
+			rs = append(rs, string(rand.Intn(26)+65))
+		} else {
+			// a-z
+			rs = append(rs, string(rand.Intn(26)+97))
+		}
+	}
+	return strings.Join(rs, "")
+
+}
+
+func RandomInt(n int) int {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	return random.Intn(n)
+}
+
+func RandomIntString(n int) string {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	return strconv.Itoa(random.Intn(n))
 }
