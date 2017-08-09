3b046f8caa175debe2ca6ae91004cd3e9342b0c1
ggguang <ggguang@uufff.com>
2017-08-09 10:12:25 +0800

    2017-08-09 添加：错误代码 200 、 422 、自定义验证错误（10000+）， 不记录日志，未开启

--- helper/grpc.go
+++ helper/grpc.go
@@ -15,6 +15,11 @@ func GrpcError(c codes.Code, format string) error {
 
 	file = fmt.Sprintf("error file : %v ( line : %d )", file, line)
 
+	// 错误代码 200 、 422 、自定义验证错误（10000+）， 不记录日志
+	//if c == 200 || c == 422 || c > 9999 {
+	//	return status.Errorf(c, format)
+	//}
+
 	logrus.Error(file)
 	logrus.Error("error info :" + format)
 
