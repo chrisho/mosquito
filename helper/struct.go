17bc2589cc7b6a917444b667abf205cb122e72a0
ggguang <ggguang@uufff.com>
2017-09-13 11:32:26 +0800

    2017-09-13 StructFieldByNameSetStringValue

--- helper/struct.go
+++ helper/struct.go
@@ -9,3 +9,24 @@ func StructFieldByNameIsValid(structPointer interface{}, fieldName string) bool 
 
 	return sElem.FieldByName(fieldName).IsValid()
 }
+
+// structPointer 必须是 struct 的 指针
+func StructFieldByNameSetStringValue(structPointer interface{}, fieldName, newValue string) (oldValue string) {
+
+	sElem := reflect.ValueOf(structPointer).Elem()
+	reflectValue := sElem.FieldByName(fieldName)
+
+	if ! reflectValue.IsValid() {
+		return oldValue
+	}
+	oldValueInterface := reflectValue.Interface()
+
+	if oldValueStr, ok := oldValueInterface.(string); ok {
+		oldValue = oldValueStr
+	} else {
+		return oldValue
+	}
+	reflectValue.SetString(newValue)
+
+	return oldValue
+}
