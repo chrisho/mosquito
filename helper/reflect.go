fe51a9ad574b9d772389cae889ea9a2adc4ca42c
ggguang <ggguang@uufff.com>
2017-09-22 20:42:50 +0800

    2017-09-22

--- helper/reflect.go
+++ helper/reflect.go
@@ -17,9 +17,13 @@ func Reflect(model interface{}, dest interface{}) {
 	}
 }
 
-//panic if s is not a slice
+// panic if s is not a slice
 func ReverseSlice(s interface{}) {
-	size := reflect.ValueOf(s).Len()
+	reflectValue := reflect.ValueOf(s)
+	if reflectValue.Kind() != reflect.Slice {
+		return
+	}
+	size := reflectValue.Len()
 	swap := reflect.Swapper(s)
 	for i, j := 0, size-1; i < j; i, j = i+1, j-1 {
 		swap(i, j)
