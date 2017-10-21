8f7277b12ec6ccddea472d9986883ca47249e128
ggguang <ggguang@uufff.com>
2017-10-21 16:12:39 +0800

    2017-10-21 time id sort 容错修改

--- helper/time_id_sort.go
+++ helper/time_id_sort.go
@@ -2,5 +2,8 @@ package helper
 
 // 默认长度为19为，time(10位）* 1E9(9位） + id， 修改长度，请修改改1E9...
 func TimeIdSort(time, id int32) int64 {
+	if time <= 0 {
+		return 0
+	}
 	return int64(time) * 1E9 + int64(id)
 }
