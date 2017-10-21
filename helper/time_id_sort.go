3d6ebc70c229f2346dda8bbb5655d20d217d0bcc
ggguang <ggguang@uufff.com>
2017-10-21 16:23:44 +0800

    2017-10-21 mysql时间排序修改

--- helper/time_id_sort.go
+++ helper/time_id_sort.go
@@ -1,9 +1,11 @@
 package helper
 
+const TimeIdSortMultiple int64 = 1E9
+
 // 默认长度为19为，time(10位）* 1E9(9位） + id， 修改长度，请修改改1E9...
 func TimeIdSort(time, id int32) int64 {
 	if time <= 0 {
 		return 0
 	}
-	return int64(time) * 1E9 + int64(id)
+	return int64(time)*TimeIdSortMultiple + int64(id)
 }
