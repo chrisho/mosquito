4b517d99885452876cd933d4e1eac38dc11079fb
ggguang <ggguang@uufff.com>
2018-02-23 17:59:56 +0800

    2018-02-23 开启日志记录

--- helper/grpc.go
+++ helper/grpc.go
@@ -5,7 +5,7 @@ import (
 	"google.golang.org/grpc/status"
 	"runtime"
 	"fmt"
-	//_ "github.com/chrisho/mosquito/alilog"
+	_ "github.com/chrisho/mosquito/alilog"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,10 +13,11 @@ import (
 func GrpcError(c codes.Code, msg string) error {
 	_, file, line, _ := runtime.Caller(1)
 
+	errorLog := fmt.Sprintf("error file : %v ( code : %d) ( line : %d ) \n error info : %v", file, int(c), line, msg)
+	fmt.Println(errorLog)
+
 	// 错误代码 200 、 422 、自定义验证错误（10000+）， 不记录日志
 	if c != 200 && c != 422 && c <= 9999 {
-		errorLog := fmt.Sprintf("error file : %v ( code : %d) ( line : %d ) \n error info : %v", file, int(c), line, msg)
-		fmt.Println(errorLog)
 		logrus.Error(errorLog)
 	}
 
